c15b4d8caf6d0d46651a475536065308d8539650
romii <romiii9735@gmail.com>
2025-07-23 15:02:52 +0900

    3일차 과제-생성

---
+++ 과제_3일차/DATABASE_3일차 과제-생성.sql
@@ -0,0 +1,22 @@
+
+-- CREATE DATABASE 3day_project
+
+
+-- employees 테이블을 생성
+-- CREATE TABLE employees (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100),
+--     position VARCHAR(100),
+--     salary DECIMAL(10, 2)
+--     );
+
+
+-- 직원 데이터를 employees에 추가
+-- INSERT INTO employee(name, position, salary)
+-- VALUES
+-- ('혜린', 'PM', 90000),
+-- ('은우', 'Frontend', 80000),
+-- ('가을', 'Backend', 92000),
+-- ('지수', 'Frontend', 78000),
+-- ('민혁', 'Frontend', 96000),
+-- ('하온', 'Backend', 130000);
