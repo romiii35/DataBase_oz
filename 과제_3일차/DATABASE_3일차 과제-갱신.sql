86340ae8eb7d7546b558a0807802093c16cfec7f
romii <romiii9735@gmail.com>
2025-07-23 15:03:35 +0900

    3일차 과제-갱신

---
+++ 과제_3일차/DATABASE_3일차 과제-갱신.sql
@@ -0,0 +1,18 @@
+
+
+-- PM 직책을 가진 모든 직원의 연봉을 10% 인상
+-- UPDATE employees
+-- SET salary = salary * 1.1
+-- WHERE position = 'PM';
+
+-- SELECT * FROM employees
+-- WHERE position = 'PM';
+
+
+
+-- 모든 Backend' 직책을 가진 직원의 연봉을 5% 인상
+-- UPDATE employees
+-- SET salary = salary * 1.05 
+-- WHERE position = 'Backend'
+
+
